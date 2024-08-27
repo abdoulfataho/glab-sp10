bbc17ebf2e6ca665dbd696326a7903025b6834b4
abdoulfataho <yamyaogofataho@gmail.com>
2024-08-27 16:02:46 -0400

    final

---
+++ Glab-sp10/src/main/java/com/example/Glab_sp10/GlabSp10Application.java
@@ -0,0 +1,13 @@
+package com.example.Glab_sp10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlabSp10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlabSp10Application.class, args);
+	}
+
+}
---
+++ Glab-sp10/src/test/java/com/example/Glab_sp10/GlabSp10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Glab_sp10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlabSp10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
